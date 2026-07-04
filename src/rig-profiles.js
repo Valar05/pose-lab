83713a6137b6759fc16bfccbed78b088a8102d4b
ValarsPhone <dclarke1005@gmail.com>
2026-07-04 04:22:44 -0500

    Adjust Meshy Ready right hand roll candidate

--- src/rig-profiles.js
+++ src/rig-profiles.js
@@ -326,7 +326,7 @@ const BASE_RIG_PROFILES = {
           enabled: true,
           mode: 'world-joint-projection',
           restRelative: true,
-          rightRollOffsetDeg: -120,
+          rightRollOffsetDeg: 0,
           leftRollOffsetDeg: -90,
         },
       },
